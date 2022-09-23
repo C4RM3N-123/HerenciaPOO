307f0ddeb718589ab84e03b2feaf043bd4b2eba6
C4RM3N-123 <carmenrosasoras@gmail.com>
2022-09-23 11:23:37 -0500

    first commit

---
+++ src/Herencia/Principal.java
@@ -0,0 +1,9 @@
+package Herencia;
+
+public class Principal {
+    public static void main(String[] args){
+        Estudiante estudiante = new Estudiante("Carla", "Soto", 20, 21199, 15.6f);
+        
+        estudiante.mostrarDatos();
+    }
+}
